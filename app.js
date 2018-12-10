c00cd30704266fad970b3f8566492b05a409f900
Yuriy Seriy <admin@iMac-Pro-Yuriy.local>
2018-12-10 20:59:15 +0300

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
+
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.APP_URL || '';
+const URL = process.env.APP_URL || 'https://<app-name>.herokuapp.com:443';
+
+const app = express();
+const bot = new TelegramBot(TOKEN);
+
+app.use(bodyParser.json());
+
+app.post(`/bot${TOKEN}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
+
+app.post('/notify', (req, res) => {
+    res.json({});
+});
+
+app.listen(PORT, () => {
+    console.log(`Express server is listening on ${PORT}`);
+});
+
+bot.on('message', msg => {
+    const {text} = msg;
+
+    if (text === '/start') {
+        bot.sendMessage(msg.chat.id, 'Hello, please ether password:');
+    } else if (text === 'qwe@123') {
+        bot.sendMessage(msg.chat.id, 'Congratulations, password is correct. Get Luchy :)');
+    }
+});
